8c8292868f7708ebd3f52f544f6c1f3adcbfb857
4444Fede <federico.vega@gm2dev.com>
2024-08-02 12:23:31 -0300

    feat(arraysTwo): add completed exercise

---
+++ arraysTwo.js
@@ -0,0 +1,30 @@
+/*
+Desarrollar una función que reciba el nombre de un alumno, las notas de sus materias, y la nota de aprobación (4/6/7) e imprima:
+Nombre del usuario
+Cantidad de materias cursadas
+Cantidad de materias aprobadas
+Las notas de las materias aprobadas
+Cantidad de materias reprobadas
+Las notas de las materias reprobadas
+Si el alumno pasó de año (aprobó todas las materias)
+*/
+
+let name = 'Pijurria'
+let grades = [4, 3, 5, 9, 10, 2, 7]
+let passingGrade = 4
+
+function reportCard(name, grades, passingGrade) {
+	let coursesTaken = grades.length
+	let coursesPassed = grades.filter(function(grade){
+		return grade >= 4
+	})
+	let amountCoursesPassed = coursesPassed.length
+	let coursesFailed = grades.filter(function(grade){
+		return grade < 4
+	})
+	let amountCoursesFailed = coursesFailed.length
+	let passedTheYear = coursesTaken === amountCoursesPassed ? 'pasó' : 'no pasó'
+
+	console.log(`Nombre: ${name} \nMaterias cursadas: ${coursesTaken} \nMaterias aprobadas: ${amountCoursesPassed} \nNotas aprobadas: ${coursesPassed} \nMaterias desaprobadas ${amountCoursesFailed} \nNotas desaprobadas: ${coursesFailed} \nPaso de año: ${passedTheYear}`)
+}
+reportCard(name, grades, passingGrade)
